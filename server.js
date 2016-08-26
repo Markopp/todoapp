88f7bd68a82768d68d4d7a525cb1e1e3e2578392
Marko Papp <marko.papp@gmail.com>
2016-08-26 22:19:30 +0200

    add underscore to server.js app

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var underscore = require ('underscore');
-
 var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json);
 
 app.get('/', function(req, res) {
+	console.log("hey");
 	res.send('Todo API Root New Line');
 });
 
