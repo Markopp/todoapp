e955501d96af9d740c2c52e853a69571f1ca61e4
Marko Papp <marko.papp@gmail.com>
2016-08-26 14:27:45 +0200

    add post for todos

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'Go outside sometimes',
-	completed: false
-}, {
-	id:2,
-	description: 'Get dishes done',
-	completed: false
-}, {
-	id: 3,
-	description: 'Get done with praksa',
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json);
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root New Line');
@@ -42,6 +34,15 @@ app.get('/todos/:id', function(req, res) {
 	} else {
 		res.status(404).send();
 	}
+});
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	
+	body.id = todoNextId++;
+
+	todos.push(body);
+	res.json(body);
 });
 
 app.listen(PORT, function () {
