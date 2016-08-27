d2a0b6850fc67d3384ae5b5f43cd4fbc77190053
Marko Papp <marko.papp@gmail.com>
2016-08-27 20:57:24 +0200

    add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+/*
+var person = {
+	name: 'Marko',
+	age: '21'
+}
+
+function updatePerson (obj) {
+	obj.age = 18;
+}
+
+
+updatePerson(person);
+console.log(person);
+*/
+
+//Array Example
+
+var grades = [15, 88];
+
+function addGrades (grades) {
+
+	grades = [33, 42, 99];
+	debugger;
+}
+
+
+addGrades(grades);
+console.log(grades);
